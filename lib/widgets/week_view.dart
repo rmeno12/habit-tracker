55e83d07c2d0753dc346a5c249f80f7d448c4cfe
Rahul Menon <menonrahul02@gmail.com>
2020-03-20 23:02:38 -0500

    Create WeekView widget

---
+++ lib/widgets/week_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WeekView extends StatefulWidget {
+  WeekView();
+
+  @override
+  WeekViewState createState() => new WeekViewState();
+}
+
+class WeekViewState extends State<WeekView> {
+  Widget build(BuildContext context) {
+    return Container();
+  } 
+}
