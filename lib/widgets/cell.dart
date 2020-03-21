41bf75b2f8d34a51ecf60c0afccc047ab1613ce6
Rahul Menon <menonrahul02@gmail.com>
2020-03-21 18:44:19 -0500

    Add support for events and event markers in cells

--- lib/widgets/cell.dart
+++ lib/widgets/cell.dart
@@ -23,20 +23,45 @@ class Cell extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(6),
-                  child: Text(
-                    _day.day.toString(),
-                    style: TextStyle(
-                      color: _isToday ? Colors.deepPurple : Colors.grey,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
+                _buildText(),
+                _buildMarkers(),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildText() {
+    return Container(
+      padding: EdgeInsets.all(6),
+      child: Text(
+        _day.day.toString(),
+        style: TextStyle(
+          color: _isToday ? Colors.deepPurple : Colors.grey,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMarkers() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: _events.map((event) => _buildMarker(event.color)).toList(),
+    );
+  }
+
+  Widget _buildMarker(Color color) {
+    return Container(
+      height: 10.0,
+      margin: EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: color,
+        borderRadius: BorderRadius.circular(4),
       ),
     );
   }
