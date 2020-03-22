749638b27059d29a121b48e70ce71bbb74c5a3a8
Rahul Menon <menonrahul02@gmail.com>
2020-03-22 11:57:37 -0500

    Add toString and fromString methods to Event class for use with databases

--- lib/widgets/event.dart
+++ lib/widgets/event.dart
@@ -6,4 +6,23 @@ class Event {
   final String value;
 
   Event({this.color, this.name, this.value});
+
+  @override
+  String toString() {
+    String out = '{';
+    out += color.value.toString() + ',';
+    out += name + ',';
+    out += value + '}';
+
+    return out;
+  }
+
+  static Event fromString(String inp) {
+    List<String> parts = inp.split(',');
+    Color color = Color(int.parse(parts[0]));
+    String name = parts[1];
+    String value = parts[2].substring(0, parts[2].length);
+
+    return Event(color: color, name: name, value: value);
+  }
 }
