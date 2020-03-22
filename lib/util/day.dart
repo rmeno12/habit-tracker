af22cf5a35ea8986d15bbf057c2e6a345019060a
Rahul Menon <menonrahul02@gmail.com>
2020-03-22 15:51:22 -0500

    Create factory constructor and helper methods for use with database

--- lib/util/day.dart
+++ lib/util/day.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:habittracker/util/event.dart';
+import 'package:intl/intl.dart';
 
 class Day {
   final DateTime date;
@@ -8,17 +9,33 @@ class Day {
 
   Day({this.date, this.eventList});
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': date,
+  factory Day.fromMap(Map<String, dynamic> json) => new Day(
+    date: DateTime.parse(json['id']),
+    eventList: getEventListFromString(json['event_list'])
+  );
+
+  Map<String, dynamic> toMap() => {
+      'id': createDateString(),
       'event_list': createEventsString(),
     };
-  }
 
   String createEventsString() {
     List<String> stringsList = eventList.map((event) => event.toString()).toList();
     String json = jsonEncode(stringsList);
 
     return json;
+  }
+
+  String createDateString() {
+    final DateFormat formatter = DateFormat('yyyy-MM-dd');
+    String dateString = formatter.format(date);
+    
+    return dateString;
+  }
+
+  static List<Event> getEventListFromString(String inp) {
+    List<Event> events = json.decode(inp).map((event) => Event.fromString(event));
+
+    return events;
   }
 }
