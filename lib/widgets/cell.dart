c8b329610b2321b46eb202584760c0d93418e1b8
Rahul Menon <menonrahul02@gmail.com>
2020-03-21 22:14:52 -0500

    Handle event overflows and make date font bolder

--- lib/widgets/cell.dart
+++ lib/widgets/cell.dart
@@ -10,14 +10,11 @@ class Cell extends StatefulWidget {
 
   const Cell({this.day, this.isToday, this.events});
 
-
   CellState createState() => new CellState();
 }
 
 class CellState extends State<Cell> {
   Widget build(BuildContext build) {
-    bool isOverflow = widget.events.length > 7;
-
     return Expanded(
       child: Card(
         child: Container(
@@ -45,6 +42,7 @@ class CellState extends State<Cell> {
         widget.day.day.toString(),
         style: TextStyle(
           color: widget.isToday ? Colors.deepPurple : Colors.grey,
+          fontWeight: FontWeight.w800,
           fontSize: 20,
         ),
       ),
@@ -52,13 +50,33 @@ class CellState extends State<Cell> {
   }
 
   Widget _buildMarkers() {
+    bool isOverflow = widget.events.length > 7;
+
     return Container(
       margin: EdgeInsets.symmetric(vertical: 3),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
-        children:
-            widget.events.map((event) => _buildMarker(event.color)).toList(),
+        children: isOverflow
+            ? widget.events.map((event) => _buildMarker(event.color)).toList()
+            : <Widget>[
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    padding: EdgeInsets.symmetric(horizontal: 4),
+                    child: Text(
+                      '+' + (widget.events.length - 4).toString(),
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  )
+                ] +
+                widget.events
+                    .sublist(0, 4)
+                    .map((event) => _buildMarker(event.color))
+                    .toList(),
       ),
     );
   }
