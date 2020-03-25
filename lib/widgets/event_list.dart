50e3a6391a7c1d7a4ae26295a6a2160afbdea82c
Rahul Menon <menonrahul02@gmail.com>
2020-03-25 13:23:11 -0500

    Create EventList widget

---
+++ lib/widgets/event_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventList extends StatefulWidget {
+  @override
+  _EventListState createState() => _EventListState();
+}
+
+class _EventListState extends State<EventList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
