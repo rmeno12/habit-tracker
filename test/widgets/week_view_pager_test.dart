84fbf8afbb89ccddcc13408a3660471b52519a80
Rahul Menon <menonrahul02@gmail.com>
2020-03-26 00:19:26 -0500

    Add StreamController to WeekViewPager constructor in test

--- test/widgets/week_view_pager_test.dart
+++ test/widgets/week_view_pager_test.dart
@@ -1,10 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:habittracker/widgets/week_view_pager.dart';
 
 void main() {
   MaterialApp app = MaterialApp(
-    home: WeekViewPager(),
+    home: WeekViewPager(
+      selectionController: StreamController<DateTime>.broadcast(),
+    ),
   );
 
   testWidgets('WeekViewPager UI Tests', (WidgetTester tester) async {
