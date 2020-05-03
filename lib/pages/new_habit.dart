4b6777106de4785d727f71a7f13247bd06164df0
Rahul Menon <menonrahul02@gmail.com>
2020-05-02 22:17:34 -0500

    Create NewHabitPage

---
+++ lib/pages/new_habit.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NewHabitPage extends StatefulWidget {
+  @override
+  _NewHabitPageState createState() => _NewHabitPageState();
+}
+
+class _NewHabitPageState extends State<NewHabitPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
