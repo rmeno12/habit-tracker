d8c14a597b21f40a521eee6bcf16790cb1e10480
Rahul Menon <menonrahul02@gmail.com>
2020-05-03 01:01:01 -0500

    Create some parts of NewHabitPage, list others

--- lib/pages/new_habit.dart
+++ lib/pages/new_habit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NewHabitPage extends StatefulWidget {
@@ -6,14 +7,83 @@ class NewHabitPage extends StatefulWidget {
 }
 
 class _NewHabitPageState extends State<NewHabitPage> {
+  final titleController = TextEditingController();
+  bool dateIncluded = false;
+  DateTime selectedDate;
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate ?? DateTime.now(),
+        firstDate: DateTime(2012),
+        lastDate: DateTime.now());
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Container(
-          child: Text('random')
-        ),
+            padding: EdgeInsets.all(12),
+            child: ListView(
+              children: <Widget>[
+                Text(
+                  'Create a new habit',
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+                TextField(
+                  controller: titleController,
+                  decoration:
+                      InputDecoration(hintText: 'Enter the name of your habit'),
+                ),
+                Text('header: values'),
+                Text('thing to add/remove types of values'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('date picker for first day'),
+                    Checkbox(
+                      value: dateIncluded,
+                      onChanged: (value) {
+                        setState(() {
+                          dateIncluded = !dateIncluded;
+                        });
+                        print(value);
+                      },
+                    )
+                  ],
+                ),
+                RaisedButton(
+                  child: Text('Select date'),
+                  onPressed: dateIncluded
+                      ? () {
+                          _selectDate(context);
+                        }
+                      : null,
+                ),
+                Text(selectedDate == null || !dateIncluded
+                    ? "No date selected"
+                    : "First day is $selectedDate"),
+                RaisedButton(
+                  // TODO: Implement submission action
+                  onPressed: () {},
+                  child: Text('Create habit'),
+                )
+              ],
+            )),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    super.dispose();
   }
 }
