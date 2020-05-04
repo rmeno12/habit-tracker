efad11551bbc64557a622f6ad82731daab910850
Rahul Menon <menonrahul02@gmail.com>
2020-05-03 22:08:06 -0500

    Create ValueTable widget

---
+++ lib/widgets/value_table.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ValueTable extends StatefulWidget {
+  @override
+  _ValueTableState createState() => _ValueTableState();
+}
+
+class _ValueTableState extends State<ValueTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
